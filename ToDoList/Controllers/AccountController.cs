e13c0f16f3d9dc9988337caac1cf34783d123c64
Elly Maimon <eliranmaimon@gmail.com>
2019-08-08 11:06:58 -0700

    Identity User Registration Controller

---
+++ ToDoList/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using ToDoList.Models;
+using System.Threading.Tasks;
+using ToDoList.ViewModels;
+
+namespace ToDoList.Controllers
+{
+  public class AccountController : Controller
+  {
+    private readonly ToDoListContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ToDoListContext db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register(RegisterViewModel model)
+    {
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
+  }
+}
