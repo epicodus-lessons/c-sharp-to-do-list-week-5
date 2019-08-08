370a587b180819342be1302fa6317ece727ea99c
Elly Maimon <eliranmaimon@gmail.com>
2019-08-08 14:08:04 -0700

    Identity User Logins

--- ToDoList/Controllers/AccountController.cs
+++ ToDoList/Controllers/AccountController.cs
@@ -43,5 +43,31 @@ namespace ToDoList.Controllers
         return View();
       }
     }
+
+    public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> LogOff()
+    {
+      await _signInManager.SignOutAsync();
+      return RedirectToAction("Index");
+    }
   }
 }
---
+++ ToDoList/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+namespace ToDoList.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
